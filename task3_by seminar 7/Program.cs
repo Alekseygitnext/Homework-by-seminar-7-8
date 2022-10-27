031f45f0b99a72967d55ff075b535e45db1c8326
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-27 23:33:25 +1000

    задача 3 к семинару № 7

---
+++ task3_by seminar 7/Program.cs
@@ -0,0 +1,64 @@
+﻿// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+PrintArray(matrix);
+
+int minSumLine = 0;
+int sumLine = SumLineElements(matrix, 0);
+for (int i = 1; i < matrix.GetLength(0); i++)
+{
+  int currentSumLine = SumLineElements(matrix, i);
+  if (sumLine > currentSumLine)
+  {
+    sumLine = currentSumLine;
+    minSumLine = i;
+  }
+}
+
+Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой элементов ({sumLine}) ");
+
+int SumLineElements(int[,] array, int i)
+{
+  int sumLine = array[i,0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sumLine += array[i,j];
+  }
+  return sumLine;
+}
+
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+void FillArray(int[,] matr)
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            matr[m, n] = new Random().Next(range);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            Console.Write($"{matr[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
