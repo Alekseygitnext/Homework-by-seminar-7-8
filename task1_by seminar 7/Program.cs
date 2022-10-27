31d07427b1440f8b66c6687f2f2a3ace3f93ac03
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-10-27 23:18:48 +1000

    задача 1 к семинару № 7

---
+++ task1_by seminar 7/Program.cs
@@ -0,0 +1,62 @@
+﻿//Написать программу, упорядочивания по убыванию элементов каждой строки двумерного массива
+
+Console.WriteLine($"\nВведите размер массива m x n и диапазон случайных значений:");
+int m = InputNumbers("Введите m: ");
+int n = InputNumbers("Введите n: ");
+int range = InputNumbers("Введите диапазон: от 1 до ");
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+void FillArray(int[,] matr)
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            matr[m, n] = new Random().Next(range);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            Console.Write($"{matr[m, n]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Ordering(int[,] matr)
+{
+    for (int m = 0; m < matr.GetLength(0); m++)
+    {
+        for (int n = 0; n < matr.GetLength(1); n++)
+        {
+            for (int k = 0; k < matr.GetLength(1) - n - 1; k++)
+            {
+                if (matr[m, k] < matr[m, k + 1])
+                {
+                    int t = matr[m, k];
+                    matr[m, k] = matr[m, k + 1];
+                    matr[m, k + 1] = t;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = new int[m, n];
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+Ordering(matrix);
+PrintArray(matrix);
